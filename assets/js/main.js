9b4053eeae22b61897e55e23d8ddb9e72ca94c7f
Ariel Itzli Molina Sanchez <itzli2000@msn.com>
2017-09-06 19:34:01 -0500

    Moddified scroll script to count navbar height

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,13 +37,15 @@ $(document).ready(function() {
       ) {
       // Figure out element to scroll to
     var target = $(this.hash);
+    var navHeight = $('#mainNav').height();
+    var scrollToPosition = target.offset().top - navHeight;
     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       // Does a scroll target exist?
       if (target.length) {
         // Only prevent default if animation is actually gonna happen
         event.preventDefault();
         $('html, body').animate({
-          scrollTop: target.offset().top
+          scrollTop: scrollToPosition
         }, 1500, function() {
           // Callback after animation
           // Must change focus!
