5d2818056f7e3a8d9582e494c614538555d77ae0
Ariel Itzli Molina Sanchez <itzli2000@msn.com>
2017-09-13 20:22:36 -0500

    Contact php is workin, tested on 1and1 server

--- contact.php
+++ contact.php
@@ -7,44 +7,45 @@ if(isset($_POST['submit'])){
         "output-charset" => $encoding,
         "line-length" => 76,
         "line-break-chars" => "\r\n"
+        );
 
 // Multiple recipients
-        $to = 'itzli2000@msn.com';
-        $from_name = $_POST['name'];
-        $from_lastname = $_POST['lastname'];
-        $from_phone = $_POST['phone'];
-        $from_mail = $_POST['email'];
-        $from_message = $_POST['message'];
+    $to = 'itzli2000@msn.com';
+    $from_name = $_POST['name'];
+    $from_lastname = $_POST['lastname'];
+    $from_phone = $_POST['phone'];
+    $from_mail = $_POST['email'];
+    $from_message = $_POST['message'];
 
 // Subject
-        $subject = 'Contacto desde mi página';
+    $subject = 'Contacto desde mi página';
 
 // Message
-        $message = '
-        <html>
-        <head>
-              <title>Mensaje prueba para envio de contenido.</title>
-          </head>
-          <body>
-              <p> Teléfono:'. $from_phone .' <br></p>
-              <p>'. $from_message .'</p>
-          </body>
-          </html>
-      ';
+    $message = '
+    <html>
+    <head>
+      <title>Mensaje prueba para envio de contenido.</title>
+  </head>
+  <body>
+      <p> Teléfono:'. $from_phone .' <br></p>
+      <p>'. $from_message .'</p>
+  </body>
+  </html>
+  ';
 
 // To send HTML mail, the Content-type header must be set
 // Mail header
-      $header = "Content-type: text/html; charset=".$encoding." \r\n";
-      $header .= "From: ".$from_name.$from_lastname." <".$from_mail."> \r\n";
-      $header .= "MIME-Version: 1.0 \r\n";
-      $header .= "Content-Transfer-Encoding: 8bit \r\n";
-      $header .= "Date: ".date("r (T)")." \r\n";
-      $header .= iconv_mime_encode("Subject", $mail_subject, $subject_preferences);
+  $header = "Content-type: text/html; charset=".$encoding." \r\n";
+  $header .= "De: ".$from_name.$from_lastname." <".$from_mail."> \r\n";
+  $header .= "MIME-Version: 1.0 \r\n";
+  $header .= "Content-Transfer-Encoding: 8bit \r\n";
+  $header .= "Fecha: ".date("r (T)")." \r\n";
+  $header .= iconv_mime_encode("Asunto", $mail_subject, $subject_preferences);
 
 // Mail it
-      mail($to, $subject, $message, $header);
+  mail($to, $subject, $message, $header);
 
 // Redirect
-      header('Location: index.html');
-  }
-  ?>
+  header('Location: index.html');
+}
+?>
