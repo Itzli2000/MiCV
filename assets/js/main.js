e7090a57cbc86f0388e8c0bfcb69d1f44eb32301
Ariel Itzli Molina Sanchez <itzli2000@msn.com>
2017-12-04 16:50:56 -0600

    fix: deleted function to validate form

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,21 +49,3 @@ $(document).ready(function() {
   });
 
 });
-
-
-
-
-// JS form validation
-(function() {
-  "use strict";
-  window.addEventListener("load", function() {
-    var form = document.getElementById("needs-validation");
-    form.addEventListener("submit", function(event) {
-      if (form.checkValidity() == false) {
-        event.preventDefault();
-        event.stopPropagation();
-      }
-      form.classList.add("was-validated");
-    }, false);
-  }, false);
-}());
